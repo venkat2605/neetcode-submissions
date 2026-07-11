f27bb6b5d26b43a86ce1f49743c5d910464b953d
Jaddu Venkata Siva Prasad <jvs75869@gmail.com>
2026-07-11 12:05:12 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const count = new Map();
+        for(let num of nums){
+        count.set(num, (count.get(num) || 0) + 1);
+        }
+
+        const buckets = Array.from({length : nums.length + 1}, () => []);
+
+        for (let [num, freq] of count) {
+        buckets[freq].push(num);
+        }
+
+        const result = [];
+
+        for (let freq = buckets.length - 1; freq >= 0; freq--) {
+            for (let num of buckets[freq]) {
+                result.push(num);
+                if (result.length == k)
+                    return result;
+            }
+        }
+    }
+}
